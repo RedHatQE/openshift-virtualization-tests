b60c206a68ea25ad805161b203360b256c9667c7
Ohad <orevah@redhat.com>
2025-01-29 11:50:53 +0200

    Added assertion to outdatedVirtualMachineInstanceWorkloads
    Added assertion to validate that outdatedVirtualMachineInstanceWorkloads
    is equal to 0 before upgrading, and after upgrade it changed to
    validate that the value of the metric is greater than zero.

--- tests/observability/upgrade/test_upgrade_observability.py
+++ tests/observability/upgrade/test_upgrade_observability.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.observability.upgrade.utils import wait_for_non_zero_metric_value
+from tests.observability.upgrade.utils import wait_for_greater_than_zero_metric_value
 from tests.observability.utils import validate_metrics_value
 from tests.upgrade_params import IUO_UPGRADE_TEST_DEPENDENCY_NODE_ID, IUO_UPGRADE_TEST_ORDERING_NODE_ID
 from utilities.constants import DEPENDENCY_SCOPE_SESSION
@@ -16,7 +16,8 @@ class TestUpgradeObservability:
     @pytest.mark.polarion("CNV-11749")
     @pytest.mark.order(before=KUBEVIRT_VMI_NUMBER_OF_OUTDATED)
     @pytest.mark.dependency(name=f"test_metric_{KUBEVIRT_VMI_NUMBER_OF_OUTDATED}")
-    def test_metric_kubevirt_vmi_number_of_outdated_before_upgrade(self, prometheus):
+    def test_metric_kubevirt_vmi_number_of_outdated_before_upgrade(self, prometheus, kubevirt_resource):
+        assert kubevirt_resource.instance.status.outdatedVirtualMachineInstanceWorkloads == 0
         validate_metrics_value(
             prometheus=prometheus,
             metric_name=KUBEVIRT_VMI_NUMBER_OF_OUTDATED,
@@ -32,7 +33,7 @@ class TestUpgradeObservability:
         scope=DEPENDENCY_SCOPE_SESSION,
     )
     def test_metric_kubevirt_vmi_number_of_outdated_after_upgrade(self, prometheus):
-        wait_for_non_zero_metric_value(
+        wait_for_greater_than_zero_metric_value(
             prometheus=prometheus,
             metric_name=KUBEVIRT_VMI_NUMBER_OF_OUTDATED,
         )
--- tests/observability/upgrade/utils.py
+++ tests/observability/upgrade/utils.py
@@ -8,7 +8,7 @@ from utilities.monitoring import get_metrics_value
 LOGGER = logging.getLogger(__name__)
 
 
-def wait_for_non_zero_metric_value(prometheus, metric_name):
+def wait_for_greater_than_zero_metric_value(prometheus, metric_name):
     samples = TimeoutSampler(
         wait_timeout=TIMEOUT_5MIN,
         sleep=TIMEOUT_30SEC,
@@ -19,7 +19,7 @@ def wait_for_non_zero_metric_value(prometheus, metric_name):
     sample = None
     try:
         for sample in samples:
-            if sample and int(sample) != 0:
+            if sample and int(sample) > 0:
                 return
     except TimeoutExpiredError:
         LOGGER.info(f"Metric value of: {metric_name} is: {sample}, expected value: non zero")
