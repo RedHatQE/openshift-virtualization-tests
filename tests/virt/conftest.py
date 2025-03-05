3acaec51bd4f33fc4f5bae03e6a9d1d510813cf1
Vasiliy Sibirskiy <vsibirsk@redhat.com>
2025-03-05 11:57:32 +0200

    run all checks and gather all failed ones in a list

--- tests/virt/conftest.py
+++ tests/virt/conftest.py
@@ -25,36 +25,32 @@ def virt_special_infra_sanity(
 ):
     """Performs verification that cluster has all required capabilities for virt special_infra marked tests."""
 
-    def _fail_verification(_message):
-        LOGGER.error(f"Special_infra cluster verification failed! {_message}")
-        exit_pytest_execution(
-            message=_message,
-            return_code=98,
-            filename="virt_special_infra_sanity_failure.txt",
-            junitxml_property=junitxml_plugin,
-        )
-
     def _verify_not_psi_cluster(_is_psi_cluster):
+        LOGGER.info("Verify running on BM cluster")
         if _is_psi_cluster:
-            _fail_verification(_message="Cluster should be BM and not PSI")
+            failed_verifications_list.append("Cluster should be BM and not PSI")
 
     def _verify_cpumanager_workers(_schedulable_nodes):
+        LOGGER.info("Verify cluster nodes have CPU Manager labels")
         if not any([node.labels.cpumanager == "true" for node in _schedulable_nodes]):
-            _fail_verification(_message="Cluster does't have CPU Manager")
+            failed_verifications_list.append("Cluster does't have CPU Manager")
 
     def _verify_gpu(_gpu_nodes, _nodes_with_supported_gpus):
+        LOGGER.info("Verify cluster nodes have enough supported GPU cards")
         if not _gpu_nodes:
-            _fail_verification(_message="Cluster doesn't have any GPU nodes")
+            failed_verifications_list.append("Cluster doesn't have any GPU nodes")
         if not _nodes_with_supported_gpus:
-            _fail_verification(_message="Cluster doesn't have any nodes with supported GPUs")
+            failed_verifications_list.append("Cluster doesn't have any nodes with supported GPUs")
         if len(_nodes_with_supported_gpus) < 2:
-            _fail_verification(_message="Cluster has only 1 node with GPU")
+            failed_verifications_list.append("Cluster has only 1 node with GPU")
 
     def _verfify_no_dpdk():
+        LOGGER.info("Verify cluster doesn't have DPDK enabled")
         if PerformanceProfile(name="dpdk").exists:
-            _fail_verification(_message="Cluster has DPDK enabled (DPDK is incomatible with NVIDIA GPU)")
+            failed_verifications_list.append("Cluster has DPDK enabled (DPDK is incomatible with NVIDIA GPU)")
 
     def _verify_numa(_schedulable_nodes, _workers_utility_pods):
+        LOGGER.info("Verify cluster has NUMA")
         cat_cmd = "cat /etc/kubernetes/kubelet.conf"
         single_numa_node_cmd = f"{cat_cmd} | grep -i single-numa-node"
         topology_manager_cmd = f"{cat_cmd} | grep -w TopologyManager"
@@ -63,25 +59,30 @@ def virt_special_infra_sanity(
                 pod_exec = ExecCommandOnPod(utility_pods=_workers_utility_pods, node=node)
                 out = pod_exec.exec(command=cmd, ignore_rc=True)
                 if not out:
-                    _fail_verification(_message=f"Cluster does not have {cmd.split()[-1]} enabled")
+                    failed_verifications_list.append(f"Cluster does not have {cmd.split()[-1]} enabled")
 
     def _verify_sriov(_sriov_workers):
+        LOGGER.info("Verify cluster has worker node with SR-IOV card")
         if not _sriov_workers:
-            _fail_verification(_message="Cluster doesn't have any SR-IOV workers")
+            failed_verifications_list.append("Cluster doesn't have any SR-IOV workers")
 
     def _verify_evmcs_support(_schedulable_nodes):
+        LOGGER.info("Verify cluster nodes support VMX cpu fixture")
         for node in _schedulable_nodes:
             if not any([
                 label == "cpu-feature.node.kubevirt.io/vmx" and value == "true" for label, value in node.labels.items()
             ]):
-                _fail_verification(_message="Cluster doens't have any node that supports VMX cpu feature")
+                failed_verifications_list.append("Cluster doens't have any node that supports VMX cpu feature")
 
     def _verify_hugepages_1gi(_workers):
+        LOGGER.info("Verify cluster has 1Gi hugepages enabled")
         if not any([
             parse_string_unsafe(worker.instance.status.allocatable["hugepages-1Gi"]) >= parse_string_unsafe("1Gi")
             for worker in _workers
         ]):
-            _fail_verification(_message="Cluster doesn't have hugepages-1Gi")
+            failed_verifications_list.append("Cluster doesn't have hugepages-1Gi")
+
+    failed_verifications_list = []
 
     if "special_infra" in pytestconfig.getoption("-m"):
         LOGGER.info("Verifying that cluster has all required capabilities for special_infra marked tests")
@@ -93,6 +94,16 @@ def virt_special_infra_sanity(
         _verify_sriov(_sriov_workers=sriov_workers)
         _verify_evmcs_support(_schedulable_nodes=schedulable_nodes)
         _verify_hugepages_1gi(_workers=workers)
+
+    if failed_verifications_list:
+        err_msg = "\n".join(failed_verifications_list)
+        LOGGER.error(f"Special_infra cluster verification failed! Missing components:\n{err_msg}")
+        exit_pytest_execution(
+            message=err_msg,
+            return_code=98,
+            filename="virt_special_infra_sanity_failure.txt",
+            junitxml_property=junitxml_plugin,
+        )
 
 
 @pytest.fixture(scope="session")
