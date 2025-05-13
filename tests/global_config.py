88faba730ca63c37d75b33a5ce2b818dd7f38e40
Jenia Peimer <jpeimer@redhat.com>
2025-05-13 13:03:37 +0300

    use dynamic vm creation, add mig b to a

--- tests/global_config.py
+++ tests/global_config.py
@@ -166,8 +166,8 @@ default_storage_class, default_storage_class_configuration = _get_default_storag
 default_volume_mode = default_storage_class_configuration["volume_mode"]
 default_access_mode = default_storage_class_configuration["access_mode"]
 
-source_storage_class_for_storage_migration = HppCsiStorageClass.Name.HOSTPATH_CSI_BASIC
-target_storage_class_for_storage_migration = StorageClassNames.CEPH_RBD_VIRTUALIZATION
+storage_class_for_storage_migration_a = HppCsiStorageClass.Name.HOSTPATH_CSI_BASIC
+storage_class_for_storage_migration_b = StorageClassNames.CEPH_RBD_VIRTUALIZATION
 
 link_aggregation_mode_matrix = [
     "active-backup",
