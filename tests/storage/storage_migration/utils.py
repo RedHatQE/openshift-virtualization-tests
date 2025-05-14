0e47a200f8dd302d1543605747c646ab9477b76f
Jenia Peimer <jpeimer@redhat.com>
2025-05-14 14:29:12 +0300

    verify VM not restarted after migration, use unpriv client

--- tests/storage/storage_migration/utils.py
+++ tests/storage/storage_migration/utils.py
@@ -3,6 +3,7 @@ from ocp_resources.virtual_machine import VirtualMachine
 
 from utilities import console
 from utilities.constants import LS_COMMAND, TIMEOUT_20SEC
+from utilities.virt import get_vm_boot_time
 
 FILE_BEFORE_STORAGE_MIGRATION = "file-before-storage-migration"
 CONTENT = "some-content"
@@ -22,3 +23,14 @@ def check_file_in_vm(vm: VirtualMachine, file_name: str, file_content: str) -> N
         vm_console.expect(file_name, timeout=TIMEOUT_20SEC)
         vm_console.sendline(f"cat {file_name}")
         vm_console.expect(file_content, timeout=TIMEOUT_20SEC)
+
+
+def verify_linux_vms_boot_time_after_storage_migration(
+    vm_list: list[VirtualMachine], initial_boot_time: dict[VirtualMachine, str]
+) -> None:
+    rebooted_vms = {}
+    for vm in vm_list:
+        current_boot_time = get_vm_boot_time(vm=vm)
+        if initial_boot_time[vm.name] != current_boot_time:
+            rebooted_vms[vm.name] = {"initial": initial_boot_time[vm.name], "current": current_boot_time}
+    assert not rebooted_vms, f"Boot time changed for VMs:\n {rebooted_vms}"
