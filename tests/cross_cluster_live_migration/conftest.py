f43f74d4be58da756ae87ba7a3cb9274f0ec2b69
Jenia Peimer <jpeimer@redhat.com>
2025-06-19 12:18:29 +0300

    check env var, add readme

--- tests/cross_cluster_live_migration/conftest.py
+++ tests/cross_cluster_live_migration/conftest.py
@@ -8,7 +8,16 @@ from utilities.constants import REMOTE_KUBECONFIG
 
 @pytest.fixture(scope="session")
 def remote_kubeconfig_export_path():
-    return os.environ.get(REMOTE_KUBECONFIG)
+    """
+    Resolve path to the remote cluster kubeconfig.
+    Fail if the environment variable is not provided or file doesn't exist.
+    """
+    path = os.environ.get(REMOTE_KUBECONFIG)
+    if not path:
+        raise ValueError(f"{REMOTE_KUBECONFIG} environment variable is not set")
+    if not os.path.isfile(path):
+        raise FileNotFoundError(f"Remote kubeconfig file not found at '{path}'")
+    return path
 
 
 @pytest.fixture(scope="session")
