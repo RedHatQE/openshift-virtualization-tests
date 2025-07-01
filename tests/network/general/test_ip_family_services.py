e23aa6f6fa0f47705879f35a56513457c4da8491
Harshitha MS <harshitha.ms@ibm.com>
2025-07-01 17:09:31 +0530

    s390x marker for network tests
    Signed-off-by: Harshitha MS <harshitha.ms@ibm.com>

--- tests/network/general/test_ip_family_services.py
+++ tests/network/general/test_ip_family_services.py
@@ -108,6 +108,7 @@ def expected_num_families_in_service(request, dual_stack_cluster):
 class TestServiceConfigurationViaManifest:
     @pytest.mark.polarion("CNV-5789")
     @pytest.mark.single_nic
+    @pytest.mark.s390x
     def test_service_with_configured_ip_families(
         self,
         running_vm_for_exposure,
@@ -120,6 +121,7 @@ class TestServiceConfigurationViaManifest:
 
     @pytest.mark.polarion("CNV-5831")
     @pytest.mark.single_nic
+    @pytest.mark.s390x
     def test_service_with_default_ip_family_policy(
         self,
         running_vm_for_exposure,
@@ -156,6 +158,7 @@ class TestServiceConfigurationViaVirtctl:
         indirect=["virtctl_expose_service", "expected_num_families_in_service"],
     )
     @pytest.mark.single_nic
+    @pytest.mark.s390x
     def test_vitrctl_expose_services(
         self,
         expected_num_families_in_service,
