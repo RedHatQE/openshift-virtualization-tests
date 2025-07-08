f893bcb40b1f0e1a9f0d7ae120d15d43d005feb4
Ohad <orevah@redhat.com>
2025-07-08 16:04:30 +0300

    Triage kubevirt_cnao_operator_up
    Once in a while this test is failing
    because sometime the image updated takes
    little time to be updated, added sampler
    to make sure the image updated before validating
    the metric value.

--- tests/observability/network/conftest.py
+++ tests/observability/network/conftest.py
@@ -5,6 +5,7 @@ import pytest
 from ocp_resources.daemonset import DaemonSet
 from ocp_resources.network_addons_config import NetworkAddonsConfig
 from pytest_testconfig import config as py_config
+from timeout_sampler import TimeoutExpiredError, TimeoutSampler
 
 from utilities.constants import (
     CLUSTER_NETWORK_ADDONS_OPERATOR,
@@ -13,7 +14,9 @@ from utilities.constants import (
     KUBEMACPOOL_MAC_CONTROLLER_MANAGER,
     LINUX_BRIDGE,
     NON_EXISTS_IMAGE,
+    TIMEOUT_4MIN,
     TIMEOUT_10MIN,
+    TIMEOUT_10SEC,
 )
 from utilities.hco import ResourceEditorValidateHCOReconcile
 from utilities.infra import (
@@ -177,7 +180,7 @@ def invalid_cnao_operator(prometheus, admin_client, hco_namespace, csv_scope_cla
         patches={csv_scope_class: bad_cnao_operator},
         list_resource_reconcile=[NetworkAddonsConfig],
     ):
-        yield
+        yield csv_scope_class
 
     linux_bridge_pods = get_pod_by_name_prefix(
         dyn_client=admin_client,
@@ -191,3 +194,24 @@ def invalid_cnao_operator(prometheus, admin_client, hco_namespace, csv_scope_cla
 
     linux_bridge_plugin_ds = DaemonSet(name=KUBE_CNI_LINUX_BRIDGE_PLUGIN, namespace=hco_namespace.name)
     linux_bridge_plugin_ds.wait_until_deployed(timeout=TIMEOUT_10MIN)
+
+
+@pytest.fixture(scope="class")
+def csv_image_updated_with_bad_image(invalid_cnao_operator):
+    def get_csv_image():
+        for deployment in invalid_cnao_operator.instance.spec.install.spec.deployments:
+            if deployment.name == CLUSTER_NETWORK_ADDONS_OPERATOR:
+                return deployment.spec.template.spec.containers[0].image
+
+    sample = TimeoutSampler(
+        wait_timeout=TIMEOUT_4MIN,
+        sleep=TIMEOUT_10SEC,
+        func=get_csv_image,
+    )
+    try:
+        for sample in sample:
+            if sample == NON_EXISTS_IMAGE:
+                return
+    except TimeoutExpiredError:
+        LOGGER.error(f"CSV image not updated after {TIMEOUT_4MIN} seconds")
+        raise
