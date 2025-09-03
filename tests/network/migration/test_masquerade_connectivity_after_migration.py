95528bdb4f19ae26a37ec796e04bef75acb7efd9
Anat Wax <awax@redhat.com>
2025-09-03 09:25:41 +0300

    Move connectivity check inside VM creation fixture
    Signed-off-by: Anat Wax <awax@redhat.com>

--- tests/network/migration/test_masquerade_connectivity_after_migration.py
+++ tests/network/migration/test_masquerade_connectivity_after_migration.py
@@ -40,6 +40,7 @@ def running_vm_static(
 @pytest.fixture(scope="module")
 def running_vm_for_migration(
     unprivileged_client,
+    running_vm_static,
     cpu_for_migration,
     namespace,
 ):
@@ -53,27 +54,21 @@ def running_vm_for_migration(
     ) as vm:
         vm.start(wait=True)
         vm.wait_for_agent_connected()
+
+        static_vm_ip = running_vm_static.vmi.interfaces[0]["ipAddress"]
+
+        vm_console_run_commands(
+            vm=vm,
+            commands=[f"ping {static_vm_ip} -c 10 -w 10"],
+            timeout=10,
+        )
+
         yield vm
-
-
-@pytest.fixture()
-def inter_vm_connectivity_exists(
-    running_vm_static,
-    running_vm_for_migration,
-):
-    static_vm_ip = running_vm_static.vmi.interfaces[0]["ipAddress"]
-
-    vm_console_run_commands(
-        vm=running_vm_for_migration,
-        commands=[f"ping {static_vm_ip} -c 10 -w 10"],
-        timeout=10,
-    )
 
 
 @pytest.fixture()
 def migrated_vmi(
     running_vm_for_migration,
-    inter_vm_connectivity_exists,
 ):
     LOGGER.info(f"Migrating {running_vm_for_migration.name}. Current node: {running_vm_for_migration.vmi.node.name}")
 
