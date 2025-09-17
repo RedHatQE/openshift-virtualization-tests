76f5c360f3b402bcb4373b8cd9585cc3939e4be2
Kate Shvaika <kshvaika@redhat.com>
2025-09-17 13:50:29 +0200

    replace skip_if_sc_volume_binding_mode_is_wffc with immediate_matrix

--- tests/storage/test_cdi_certificate.py
+++ tests/storage/test_cdi_certificate.py
@@ -186,11 +186,10 @@ def test_dv_delete_from_vm(
 @pytest.mark.sno
 @pytest.mark.polarion("CNV-3667")
 def test_upload_after_certs_renewal(
-    skip_if_sc_volume_binding_mode_is_wffc,
+    storage_class_matrix_immediate_matrix__module__,
     refresh_cdi_certificates,
     download_image,
     namespace,
-    storage_class_name_scope_module,
 ):
     """
     Check that CDI can do upload operation after certs get refreshed
@@ -201,7 +200,7 @@ def test_upload_after_certs_renewal(
         name=dv_name,
         size="1Gi",
         image_path=LOCAL_QCOW2_IMG_PATH,
-        storage_class=storage_class_name_scope_module,
+        storage_class=[*storage_class_matrix_immediate_matrix__module__][0],
         insecure=True,
     ) as res:
         check_upload_virtctl_result(result=res)
@@ -251,7 +250,7 @@ def test_import_clone_after_certs_renewal(
 @pytest.mark.sno
 @pytest.mark.polarion("CNV-3977")
 def test_upload_after_validate_aggregated_api_cert(
-    skip_if_sc_volume_binding_mode_is_wffc,
+    storage_class_matrix_immediate_matrix__module__,
     valid_aggregated_api_client_cert,
     namespace,
     storage_class_name_scope_module,
@@ -266,7 +265,7 @@ def test_upload_after_validate_aggregated_api_cert(
         name=dv_name,
         size="1Gi",
         image_path=LOCAL_QCOW2_IMG_PATH,
-        storage_class=storage_class_name_scope_module,
+        storage_class=[*storage_class_matrix_immediate_matrix__module__][0],
         insecure=True,
     ) as res:
         check_upload_virtctl_result(result=res)
