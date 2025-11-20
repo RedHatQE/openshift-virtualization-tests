fbe73976f6a511a187ec736f1c989197cfc45347
Ohad <orevah@redhat.com>
2025-11-20 15:49:39 +0200

    Remove quarantine from runbook url test.
    Removed QUARANTINED marker from the tests test_runbook_url
    since the alerts merged to d/s and now the tests pass.

--- tests/observability/runbook_url/test_runbook_url.py
+++ tests/observability/runbook_url/test_runbook_url.py
@@ -3,9 +3,31 @@ import logging
 import pytest
 
 from tests.utils import validate_runbook_url_exists
-from utilities.constants import CNV_PROMETHEUS_RULES, QUARANTINED
+from utilities.constants import CNV_PROMETHEUS_RULES
 
 LOGGER = logging.getLogger(__name__)
+
+
+def validate_downstream_runbook_url(runbook_urls_from_prometheus_rule: dict[str, str]) -> None:
+    error_messages = {}
+    alerts_without_runbook = []
+
+    for alert_name, runbook_url in runbook_urls_from_prometheus_rule:
+        if not runbook_url:
+            LOGGER.error(f"For alert: {alert_name} Url not found")
+            alerts_without_runbook.append(alert_name)
+        error = validate_runbook_url_exists(url=runbook_url)
+        if error:
+            LOGGER.error(f"Alert {alert_name} url {runbook_url} is not valid")
+            error_messages[alert_name] = runbook_url
+    if alerts_without_runbook:
+        LOGGER.error(f"Runbook url missing for following CNV alerts: {alerts_without_runbook}")
+        raise AssertionError("CNV alerts with missing runbook url found.")
+
+    if error_messages:
+        message = f"Downstream runbook url validation failed for the followings: {error_messages}"
+        LOGGER.error(message)
+        raise AssertionError(message)
 
 
 class TestRunbookUrlsAndPrometheusRules:
@@ -19,28 +41,8 @@ class TestRunbookUrlsAndPrometheusRules:
             f"New cnv prometheusrule found: {set(cnv_prometheus_rules_names) - set(CNV_PROMETHEUS_RULES)}"
         )
 
-    @pytest.mark.xfail(
-        reason=f"{QUARANTINED}: New alerts runbooks added to upstream and not merged yet for downstream, CNV-67890",
-        run=False,
-    )
     @pytest.mark.polarion("CNV-10084")
     def test_runbook_downstream_urls(self, cnv_alerts_runbook_urls_from_prometheus_rule):
-        error_messages = {}
-        alerts_without_runbook = []
-
-        for alert_name, runbook_url in cnv_alerts_runbook_urls_from_prometheus_rule.items():
-            if not runbook_url:
-                LOGGER.error(f"For alert: {alert_name} Url not found")
-                alerts_without_runbook.append(alert_name)
-            error = validate_runbook_url_exists(url=runbook_url)
-            if error:
-                LOGGER.error(f"Alert {alert_name} url {runbook_url} is not valid")
-                error_messages[alert_name] = runbook_url
-        if alerts_without_runbook:
-            LOGGER.error(f"Runbook url missing for following CNV alerts: {alerts_without_runbook}")
-            raise AssertionError("CNV alerts with missing runbook url found.")
-
-        if error_messages:
-            message = f"Downstream runbook url validation failed for the followings: {error_messages}"
-            LOGGER.error(message)
-            raise AssertionError(message)
+        validate_downstream_runbook_url(
+            runbook_urls_from_prometheus_rule=cnv_alerts_runbook_urls_from_prometheus_rule.items()
+        )
