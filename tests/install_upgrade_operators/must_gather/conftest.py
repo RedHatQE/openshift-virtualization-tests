5d8f84b4b01166c67ca67ff966b3192ff3e3b27c
Anat Wax <awax@redhat.com>
2025-11-30 17:02:45 +0200

    infra/tests, net, nncp: require explicit client for NNCP
    Make NNCP-related classes take a mandatory DynamicClient and update all
    NNCP-related fixtures to pass an admin_client explicitly (no implicit
    default clients).
    
    Signed-off-by: Anat Wax <awax@redhat.com>

--- tests/install_upgrade_operators/must_gather/conftest.py
+++ tests/install_upgrade_operators/must_gather/conftest.py
@@ -161,12 +161,13 @@ def must_gather_nad(must_gather_bridge, node_gather_unprivileged_namespace, work
 
 
 @pytest.fixture(scope="package")
-def must_gather_bridge(worker_node1):
+def must_gather_bridge(admin_client, worker_node1):
     with network_device(
         interface_type=LINUX_BRIDGE,
         nncp_name="must-gather-br",
         interface_name="mg-br1",
         node_selector=get_node_selector_dict(node_selector=worker_node1.hostname),
+        client=admin_client,
     ) as br:
         yield br
 
