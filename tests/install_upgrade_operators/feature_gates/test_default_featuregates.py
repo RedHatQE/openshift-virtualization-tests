e66d5ea7fa8ae1fb90ebfa596b6522e930e04313
Alex <albarker@redhat.com>
2025-12-08 15:21:00 -0500

    refactored sorted list comparisons to set comparison

--- tests/install_upgrade_operators/feature_gates/test_default_featuregates.py
+++ tests/install_upgrade_operators/feature_gates/test_default_featuregates.py
@@ -83,7 +83,4 @@ def test_default_featuregates_by_resource(
     resource_object_value_by_key,
 ):
     error_message = f"Expected featuregates: {expected}, actual: {resource_object_value_by_key}"
-    if isinstance(expected, list):
-        assert sorted(expected) == sorted(resource_object_value_by_key), error_message
-    else:
-        assert expected == resource_object_value_by_key, error_message
+    assert expected == resource_object_value_by_key, error_message
