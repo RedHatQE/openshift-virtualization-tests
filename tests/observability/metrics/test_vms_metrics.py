dbaaa4441db2b3141149dadef021a44adc93b33a
Alex <albarker@redhat.com>
2025-12-16 09:11:01 -0500

    created initial test

--- tests/observability/metrics/test_vms_metrics.py
+++ tests/observability/metrics/test_vms_metrics.py
@@ -17,7 +17,7 @@ from tests.observability.metrics.constants import (
     KUBEVIRT_VM_CREATED_BY_POD_TOTAL,
     KUBEVIRT_VM_DISK_ALLOCATED_SIZE_BYTES,
     KUBEVIRT_VMI_PHASE_TRANSITION_TIME_FROM_DELETION_SECONDS_SUM_SUCCEEDED,
-    KUBEVIRT_VNC_ACTIVE_CONNECTIONS_BY_VMI,
+    KUBEVIRT_VNC_ACTIVE_CONNECTIONS_BY_VMI, KUBEVIRT_VMI_PHASE_TRANSITION_TIME_FROM_DELETION_SECONDS_COUNT_SUCCEEDED,
 )
 from tests.observability.metrics.utils import (
     compare_metric_file_system_values_with_vm_file_system_values,
@@ -511,6 +511,27 @@ class TestVmiPhaseTransitionFromDeletion:
             prometheus=prometheus,
             metric_name=KUBEVIRT_VMI_PHASE_TRANSITION_TIME_FROM_DELETION_SECONDS_SUM_SUCCEEDED,
             initial_value=initial_metric_value,
+        )
+
+
+class TestVmiPhaseTransitionFromDeletionCount:
+    @pytest.mark.parametrize(
+        "initial_metric_value",
+        [
+            pytest.param(
+                KUBEVIRT_VMI_PHASE_TRANSITION_TIME_FROM_DELETION_SECONDS_COUNT_SUCCEEDED,
+                marks=pytest.mark.polarion("CNV-12469"),
+            )
+        ],
+        indirect=True,
+    )
+    def test_kubevirt_vmi_phase_transition_from_deletion_seconds_count(
+        self, prometheus, initial_metric_value, running_metric_vm, deleted_vmi
+    ):
+        validate_metrics_value(
+            prometheus=prometheus,
+            metric_name=KUBEVIRT_VMI_PHASE_TRANSITION_TIME_FROM_DELETION_SECONDS_COUNT_SUCCEEDED,
+            expected_value=str(initial_metric_value+1),
         )
 
 
