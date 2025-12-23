f189798e059006cbd3222662ea92b35a510fa701
Kate Shvaika <kshvaika@redhat.com>
2025-12-23 14:48:59 +0100

    revert function cirros_vm_name

--- tests/storage/conftest.py
+++ tests/storage/conftest.py
@@ -401,6 +401,11 @@ def hpp_daemonset_scope_module(hco_namespace, hpp_cr_suffix_scope_module):
 
 
 @pytest.fixture()
+def cirros_vm_name(request):
+    return request.param["vm_name"]
+
+
+@pytest.fixture()
 def rhel_vm_name(request):
     return request.param["vm_name"]
 
