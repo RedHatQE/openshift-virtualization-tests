ecbf01856daf0cf73f78e56784acf99f1d1df4f6
Harel Meir <hmeir@redhat.com>
2025-12-23 14:39:15 +0200

    Add missing resources
    Signed-off-by: Harel Meir <hmeir@redhat.com>

--- tests/install_upgrade_operators/product_install/conftest.py
+++ tests/install_upgrade_operators/product_install/conftest.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 import bitmath
-from ocp_resources.storage_class import StorageClass
 import pytest
 from ocp_resources.cluster_service_version import ClusterServiceVersion
 from ocp_resources.hostpath_provisioner import HostPathProvisioner
@@ -10,6 +9,7 @@ from ocp_resources.hyperconverged import HyperConverged
 from ocp_resources.installplan import InstallPlan
 from ocp_resources.persistent_volume import PersistentVolume
 from ocp_resources.resource import get_client
+from ocp_resources.storage_class import StorageClass
 from pytest_testconfig import py_config
 
 from tests.install_upgrade_operators.product_install.constants import (
@@ -314,6 +314,7 @@ def cnv_version_to_install_info(is_production_source, ocp_current_version, cnv_i
     if not latest_z_stream:
         pytest.exit(reason="CNV version can't be determined for this run", returncode=INSTALLATION_VERSION_MISMATCH)
     return latest_z_stream
+
 
 @pytest.fixture
 def storage_class_ocs_virt(admin_client):
