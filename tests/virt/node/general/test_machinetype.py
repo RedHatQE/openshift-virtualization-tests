b3aefbc374047ec24c5ae2157248275422b2a059
Denys Shchedrivyi <dshchedr@redhat.com>
2025-12-24 18:18:05 -0800

    Remove calling function from common part
    Remove calling function from common part

--- tests/virt/node/general/test_machinetype.py
+++ tests/virt/node/general/test_machinetype.py
@@ -2,19 +2,15 @@ import logging
 
 import pytest
 from kubernetes.dynamic.exceptions import UnprocessibleEntityError
+from ocp_resources.data_source import DataSource
 from ocp_resources.template import Template
 
 from tests.virt.constants import MachineTypesNames
-from tests.virt.utils import validate_machine_type
+from tests.virt.utils import get_data_volume_template_dict_with_default_storage_class, validate_machine_type
 from utilities.constants import (
-    DATA_SOURCE_STR,
-    DV_SIZE_STR,
     FLAVOR_STR,
-    IMAGE_PATH_STR,
     OS_STR,
-    TEMPLATE_LABELS_STR,
     WORKLOAD_STR,
-    Images,
 )
 from utilities.hco import is_hco_tainted, update_hco_annotations
 from utilities.virt import (
@@ -31,15 +27,10 @@ pytestmark = pytest.mark.post_upgrade
 LOGGER = logging.getLogger(__name__)
 
 
-RHEL_8_10 = {
-    IMAGE_PATH_STR: f"{Images.Rhel.DIR}/{Images.Rhel.RHEL8_10_IMG}",
-    DV_SIZE_STR: "20Gi",
-    TEMPLATE_LABELS_STR: {
-        OS_STR: "rhel8.10",
-        WORKLOAD_STR: "server",
-        FLAVOR_STR: "tiny",
-    },
-    DATA_SOURCE_STR: "rhel8",
+RHEL_8_10_TEMPLATE_LABELS = {
+    OS_STR: "rhel8.10",
+    WORKLOAD_STR: "server",
+    FLAVOR_STR: "tiny",
 }
 
 
@@ -59,15 +50,15 @@ def vm_for_machine_type_test(request, cluster_cpu_model_scope_class, unprivilege
 
 
 @pytest.fixture()
-def vm_for_legacy_machine_type_test(
-    unprivileged_client, namespace, golden_image_data_volume_template_for_test_scope_function
-):
+def vm_for_legacy_machine_type_test(admin_client, unprivileged_client, namespace, golden_images_namespace):
     with VirtualMachineForTestsFromTemplate(
         name="vm-legacy-machine-type-test",
         namespace=namespace.name,
         client=unprivileged_client,
-        labels=Template.generate_template_labels(**RHEL_8_10["template_labels"]),
-        data_volume_template=golden_image_data_volume_template_for_test_scope_function,
+        labels=Template.generate_template_labels(**RHEL_8_10_TEMPLATE_LABELS),
+        data_volume_template=get_data_volume_template_dict_with_default_storage_class(
+            DataSource(client=admin_client, name="rhel8", namespace=golden_images_namespace.name)
+        ),
         machine_type=MachineTypesNames.pc_i440fx_rhel7_6,
     ) as vm:
         running_vm(vm=vm)
@@ -138,25 +129,6 @@ def test_pc_q35_vm_machine_type(vm_for_machine_type_test, expected):
 @pytest.mark.usefixtures("vm_for_machine_type_test")
 @pytest.mark.gating
 class TestMachineType:
-    @pytest.mark.arm64
-    @pytest.mark.gating
-    @pytest.mark.conformance
-    @pytest.mark.polarion("CNV-5658")
-    def test_major_release_machine_type(self, machine_type_from_kubevirt_config):
-        # CNV should always use a major release for machine type, for example: pc-q35-rhel9.6.0
-        assert machine_type_from_kubevirt_config.endswith(".0"), (
-            f"Machine type should be a major release {machine_type_from_kubevirt_config}"
-        )
-
-    @pytest.mark.conformance
-    @pytest.mark.polarion("CNV-8561")
-    def test_machine_type_as_rhel_9_6(self, machine_type_from_kubevirt_config):
-        """Verify that machine type in KubeVirt CR match the value pc-q35-rhel9.6.0"""
-        assert machine_type_from_kubevirt_config == MachineTypesNames.pc_q35_rhel9_6, (
-            f"Machine type value is {machine_type_from_kubevirt_config} "
-            f"does not match with {MachineTypesNames.pc_q35_rhel9_6}"
-        )
-
     @pytest.mark.arm64
     @pytest.mark.conformance
     @pytest.mark.polarion("CNV-3312")
@@ -237,11 +209,31 @@ def test_unsupported_machine_type(namespace, unprivileged_client):
             pytest.fail("VM created with invalid machine type.")
 
 
+@pytest.mark.arm64
+@pytest.mark.gating
+@pytest.mark.conformance
+@pytest.mark.polarion("CNV-5658")
+def test_major_release_machine_type(machine_type_from_kubevirt_config):
+    # CNV should always use a major release for machine type, for example: pc-q35-rhel8.3.0
+    assert machine_type_from_kubevirt_config.endswith(".0"), (
+        f"Machine type should be a major release {machine_type_from_kubevirt_config}"
+    )
+
+
+@pytest.mark.gating
+@pytest.mark.polarion("CNV-8561")
+def test_machine_type_as_rhel_9_6(machine_type_from_kubevirt_config):
+    """Verify that machine type in KubeVirt CR match the value pc-q35-rhel9.6.0"""
+    assert machine_type_from_kubevirt_config == MachineTypesNames.pc_q35_rhel9_6, (
+        f"Machine type value is {machine_type_from_kubevirt_config} "
+        f"does not match with {MachineTypesNames.pc_q35_rhel9_6}"
+    )
+
+
 @pytest.mark.parametrize(
-    "golden_image_data_source_for_test_scope_function, updated_kubevirt_config_machine_type",
+    "updated_kubevirt_config_machine_type",
     [
         pytest.param(
-            {"os_dict": RHEL_8_10},
             {"emulatedMachines": ["q35*", "pc-q35*", MachineTypesNames.pc_i440fx_rhel7_6]},
             marks=pytest.mark.polarion("CNV-7311"),
         )
