ed0df70bf1ab2a141ecd8dfeb632e1e5fed16203
Asia Khromov <azhivovk@redhat.com>
2026-01-06 16:32:41 +0200

    net, ipv6: Adjust test_service_with_configured_ip_families
    Problem:
    `test_service_with_configured_ip_families` is
    collected on IPv6 single-stack lane and fails
    during setup with an api exception when creating
    single stack service:
    `Invalid value: \"IPv4\": not configured on this cluster`
    
    Fix:
    Update the test to run with both ip families
    on dual-stack clusters and only with the IPv6
    ip family on IPv6 single-stack clusters.
    
    Signed-off-by: Asia Khromov <azhivovk@redhat.com>

--- tests/network/ip_family_services/test_service_config_manifest.py
+++ tests/network/ip_family_services/test_service_config_manifest.py
@@ -6,32 +6,42 @@ import pytest
 
 from tests.network.ip_family_services.libipfamilyservices import SERVICE_IP_FAMILY_POLICY_SINGLE_STACK
 
-SINGLE_STACK_SERVICE_IP_FAMILY = "IPv4"
-
 
 @pytest.mark.gating
-@pytest.mark.s390x
 class TestServiceConfigurationViaManifest:
-    @pytest.mark.polarion("CNV-5789")
     @pytest.mark.single_nic
-    # Not marked as `conformance`; requires NMState
+    @pytest.mark.parametrize(
+        "single_stack_service_ip_family, single_stack_service",
+        [
+            pytest.param("IPv4", "IPv4", marks=[pytest.mark.ipv4, pytest.mark.polarion("CNV-5789")]),
+            pytest.param("IPv6", "IPv6", marks=[pytest.mark.ipv6, pytest.mark.polarion("CNV-12557")]),
+        ],
+        indirect=["single_stack_service"],
+    )
     def test_service_with_configured_ip_families(
         self,
         running_vm_for_exposure,
+        single_stack_service_ip_family,
         single_stack_service,
     ):
-        assert (
-            len(running_vm_for_exposure.custom_service.instance.spec.ipFamilies) == 1
-            and running_vm_for_exposure.custom_service.instance.spec.ipFamilies[0] == SINGLE_STACK_SERVICE_IP_FAMILY
-        ), "Wrong ipFamilies set in service"
+        ip_families_in_svc = running_vm_for_exposure.custom_service.instance.spec.ipFamilies
+
+        assert len(ip_families_in_svc) == 1 and ip_families_in_svc[0] == single_stack_service_ip_family, (
+            f"Wrong ipFamilies config in service on VM {running_vm_for_exposure.name}: "
+            f"Expected: single stack {single_stack_service_ip_family} family, "
+            f"Actual: {len(ip_families_in_svc)} ip families: {ip_families_in_svc} "
+        )
 
     @pytest.mark.polarion("CNV-5831")
     @pytest.mark.single_nic
+    @pytest.mark.usefixtures("default_ip_family_policy_service")
     def test_service_with_default_ip_family_policy(
         self,
         running_vm_for_exposure,
-        default_ip_family_policy_service,
     ):
-        assert (
-            running_vm_for_exposure.custom_service.instance.spec.ipFamilyPolicy == SERVICE_IP_FAMILY_POLICY_SINGLE_STACK
-        ), "Service created with wrong default ipfamilyPolicy."
+        ip_family_policy = running_vm_for_exposure.custom_service.instance.spec.ipFamilyPolicy
+        assert ip_family_policy == SERVICE_IP_FAMILY_POLICY_SINGLE_STACK, (
+            f"Service created with wrong default ipfamilyPolicy on VM {running_vm_for_exposure.name}: "
+            f"Expected: {SERVICE_IP_FAMILY_POLICY_SINGLE_STACK},"
+            f"Actual: {ip_family_policy}"
+        )
