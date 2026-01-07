6bf573aeecfb0290219a657a8ad04a740e2abaec
Ahmad Hafe <ahafe@redhat.com>
2026-01-07 22:57:58 +0200

    fix test_clone_from_block_to_fs_using_dv_template

--- tests/storage/cdi_clone/test_clone.py
+++ tests/storage/cdi_clone/test_clone.py
@@ -2,7 +2,10 @@
 Clone tests
 """
 
+import math
+
 import pytest
+from bitmath import parse_string_unsafe
 from ocp_resources.datavolume import DataVolume
 
 from tests.os_params import FEDORA_LATEST, WINDOWS_11, WINDOWS_11_TEMPLATE_LABELS
@@ -23,7 +26,6 @@ from utilities.storage import (
     create_dv,
     create_vm_from_dv,
     data_volume_template_dict,
-    overhead_size_for_dv,
 )
 from utilities.virt import (
     VirtualMachineForTests,
@@ -278,8 +280,9 @@ def test_clone_from_block_to_fs_using_dv_template(
     namespace,
     cirros_dv_with_block_volume_mode,
     storage_class_with_filesystem_volume_mode,
-    default_fs_overhead,
 ):
+    source_pvc = cirros_dv_with_block_volume_mode.pvc
+    pvc_size = parse_string_unsafe(s=source_pvc.instance.spec.resources.requests.storage)
     create_vm_from_clone_dv_template(
         vm_name="vm-5608",
         dv_name="dv-5608",
@@ -287,10 +290,6 @@ def test_clone_from_block_to_fs_using_dv_template(
         source_dv=cirros_dv_with_block_volume_mode,
         client=unprivileged_client,
         volume_mode=DataVolume.VolumeMode.FILE,
-        # add fs overhead and round up the result
-        size=overhead_size_for_dv(
-            image_size=int(cirros_dv_with_block_volume_mode.size[:-2]),
-            overhead_value=default_fs_overhead,
-        ),
+        size=f"{math.ceil(pvc_size.to_GiB().value)}Gi",
         storage_class=storage_class_with_filesystem_volume_mode,
     )
