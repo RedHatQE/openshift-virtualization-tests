73d58956e3d4c362c30f873aea1f4ac9d3c7bdd6
Alex <albarker@redhat.com>
2026-02-03 10:15:17 -0500

    fixed metric results parsing to be safer

--- tests/install_upgrade_operators/json_patch/utils.py
+++ tests/install_upgrade_operators/json_patch/utils.py
@@ -63,9 +63,12 @@ def get_metrics_value_with_annotation(prometheus, query_string, component_name):
 def filter_metric_by_component(metrics, metric_name, component_name):
     annotation_name = get_annotation_name_for_component(component_name=component_name)
     for metric in metrics:
-        if metric["metric"]["annotation_name"] == annotation_name and metric["metric"]["__name__"] == metric_name:
-            return int(metric["value"][1])
-    LOGGER.warning(f"Metric {metric_name} with annotation {annotation_name} not found in metrics {metrics}")
+        if (
+            metric.get("metric", {}).get("annotation_name") == annotation_name
+            and metric.get("metric", {}).get("__name__") == metric_name
+        ):
+            return int(metric.get("value")[1])
+    LOGGER.warning(f"No results found when filtering for {metric_name} and annotation {annotation_name} in {metrics}.")
     return 0
 
 
@@ -80,7 +83,7 @@ def wait_for_metrics_value_update(prometheus, component_name, query_string, prev
     )
     try:
         for sample in samples:
-            if sample == previous_value + 1:
+            if sample and sample == previous_value + 1:
                 return sample
     except TimeoutExpiredError:
         LOGGER.error(f"Query string: {query_string} for component: {component_name}, previous value: {previous_value}.")
