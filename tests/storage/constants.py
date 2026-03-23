2215419d47fbc119df47563c51c62e3e36f4366d
Jenia Peimer <jpeimer@redhat.com>
2026-03-23 23:47:22 +0200

    [CCLM] Add tests for Windows VM

--- tests/storage/constants.py
+++ tests/storage/constants.py
@@ -22,3 +22,6 @@ QUAY_FEDORA_CONTAINER_IMAGE = f"docker://{Images.Fedora.FEDORA_CONTAINER_IMAGE}"
 
 TEST_FILE_NAME = "test-file.txt"
 TEST_FILE_CONTENT = "test-content"
+
+WINDOWS_TEST_FILE_NAME = "C:\\test-file.txt"
+WINDOWS_TEST_FILE_CONTENT = "test-content-windows"
