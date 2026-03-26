9be898bbfcfb260ee04437c8b40aae12dc0a3afd
Jenia Peimer <86722603+jpeimer@users.noreply.github.com>
2026-03-26 13:54:33 +0200

    Apply suggestion from @jpeimer

--- tests/storage/restricted_namespace_cloning/conftest.py
+++ tests/storage/restricted_namespace_cloning/conftest.py
@@ -28,7 +28,6 @@ from tests.storage.restricted_namespace_cloning.constants import (
 from tests.storage.utils import (
     create_cluster_role,
     create_role_binding,
-    get_dv_size_from_datasource,
     set_permissions,
 )
 from utilities.constants import OS_FLAVOR_FEDORA, PVC, UNPRIVILEGED_USER, Images
