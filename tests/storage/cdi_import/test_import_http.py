8f92faa79bac4660c81a5773bf490ea457652b9a
Adam Cinko <acinko@redhat.com>
2026-05-04 09:45:52 +0200

    fix the run VM sequence
    Signed-off-by: Adam Cinko <acinko@redhat.com>

--- tests/storage/cdi_import/test_import_http.py
+++ tests/storage/cdi_import/test_import_http.py
@@ -31,6 +31,7 @@ from utilities.constants import (
     TIMEOUT_1MIN,
     TIMEOUT_2MIN,
     TIMEOUT_5MIN,
+    TIMEOUT_40MIN,
     U1_LARGE,
     WINDOWS_2K22_PREFERENCE,
     Images,
@@ -372,6 +373,6 @@ def test_successful_vm_from_imported_dv_windows_with_vtpm(
         },
         cpu_model=cpu_for_migration,
     ) as vm:
-        running_vm(vm=vm, check_ssh_connectivity=False)
-        wait_for_windows_vm(vm=vm, version="2022")
+        vm.start()
+        wait_for_windows_vm(vm=vm, version="2022", timeout=TIMEOUT_40MIN)
         validate_os_info_vmi_vs_windows_os(vm=vm)
