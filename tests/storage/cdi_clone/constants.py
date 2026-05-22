37beb5b4afd1aa8c358de55e780e3d3302107439
Adam Cinko <acinko@redhat.com>
2026-05-22 08:16:58 +0200

    change timeout to 40 minutes
    Signed-off-by: Adam Cinko <acinko@redhat.com>

--- tests/storage/cdi_clone/constants.py
+++ tests/storage/cdi_clone/constants.py
@@ -1,5 +1,5 @@
 """Constants for CDI clone tests."""
 
-from utilities.constants import TIMEOUT_30MIN
+from utilities.constants import TIMEOUT_40MIN
 
-WINDOWS_CLONE_TIMEOUT = TIMEOUT_30MIN
+WINDOWS_CLONE_TIMEOUT = TIMEOUT_40MIN
